f62b1972489eacdea4bf1e5f77f61bf0c9148cc0
seriewe <sergej.riewe@gmail.com>
2021-05-31 16:21:40 +0200

    change gulp file to work from plugins folder

--- generators/devenv/templates/src/custom/plugins/gulpfile.js
+++ generators/devenv/templates/src/custom/plugins/gulpfile.js
@@ -5,9 +5,26 @@ const { series } = require('gulp');
 const del = require('del');
 var fs = require('fs');
 
+function getAppName(){
+    var appName, i = process.argv.indexOf("--app");
+    if(i>-1) {
+        appName = process.argv[i+1].replace(/\\/g, '');
+    }
+    return appName;
+}
+
 function createtmp(){
 
-    var json = JSON.parse(fs.readFileSync('./composer.json'));
+    var appName = getAppName();
+    if(!appName) {
+        console.log("usage: gulp bundle --app [AppFolderName]" );
+    }
+    // change directory to plugin folder
+    process.chdir("./" + appName);
+    
+    var appFolderName = path.basename(process.cwd());  
+    var json = JSON.parse(fs.readFileSync('./composer.json'));    
+    var destPath = './tmp/'+ appFolderName + "-" + json.version + '/' + appFolderName ;
 
     return gulp.src(['./**', 
               '!./{.git, .git/**}', 
@@ -16,21 +33,38 @@ function createtmp(){
               '!./package.json',
               '!./package-lock.json',
               '!./node_modules/**'])
-        .pipe(gulp.dest('./tmp/'+ path.basename(path.dirname(__filename))+ "-" + json.version + '/' + path.basename(path.dirname(__filename)) ));
+        .pipe(gulp.dest(destPath));
 }
 
 function zipplugin(){
+    
+    var appName = getAppName();
+    if(!appName) {
+        console.log("usage: gulp bundle --app [AppFolderName]" );
+    }
+    var currentFolderName = path.basename(process.cwd());
 
     var json = JSON.parse(fs.readFileSync('./composer.json'));
     process.chdir('./tmp');
+    
+    var zipSrc = './'+ currentFolderName + "-" + json.version + '/**';
+    // console.log("zipSrc: ");
+    // console.log(zipSrc);
 
-    return gulp.src(['./'+ path.basename(path.dirname(__filename))+ "-" + json.version + '/**', '!./tmp/**'])
-        .pipe(zip(path.basename(path.dirname(__filename))+ "-" + json.version + '.zip'))
-        .pipe(gulp.dest('../../'));
+    var zipTarget = currentFolderName + "-" + json.version + '.zip';
+    // console.log("zipTarget");
+    // console.log(zipTarget);
+
+    return gulp.src([zipSrc, '!./tmp/**'])
+        .pipe(zip(zipTarget))
+        .pipe(gulp.dest('../../'));    
 }
 
 function cleanup(){
+
+    console.log("cleanup: ");
     process.chdir('..');
+    console.log(process.cwd());
     return del('./tmp', {force:true});
 }
 
