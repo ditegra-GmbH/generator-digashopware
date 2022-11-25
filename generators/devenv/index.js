53f654c412451eda0217ed99c7c20945d73d4f50
Sergej <sergej.riewe@ditegra.de>
2022-11-25 17:44:35 +0100

    fix parameter

--- generators/devenv/index.js
+++ generators/devenv/index.js
@@ -25,7 +25,7 @@ module.exports = class extends Generator {
         let name = this.answers.name;
         let image = this.answers.dockwareimage;
         if(!image){
-            image = 'dockware/essentials:latest';
+            image = 'essentials:latest';
         }
         
         this.fs.copyTpl(
